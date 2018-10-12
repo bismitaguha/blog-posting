da6cfc2b83be364f7c2c6443bab19210defaa1e4
Dragneel7 <sainisurya1@gmail.com>
2018-10-12 16:29:24 +0530

    Add application skeleton

---
+++ blog.php
@@ -0,0 +1,16 @@
+<html>
+
+<head>
+    <title>IMG Workshop '18</title>
+    <link rel="stylesheet" href="blog.css">
+</head>
+
+<body>
+
+<nav class="navbar navbar-expand-sm">
+        <a class="navbar-brand" href="https://img.channeli.in"> <h3>Workshop '18</h3> </a>
+        <a href="add_blog.html"><h3>Create</h3></a>
+</nav>
+
+</body>
+</html>
